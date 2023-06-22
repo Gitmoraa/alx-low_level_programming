8831d610e2315dad111c40173feebc52cd6521dd
Gitmoraa <moraabethsheba920@gmail.com>
2023-06-22 05:15:25 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -10,5 +11,5 @@ int main(void)
  positive_or_negative(i);
  return (0);
 }
-}	
+	
 
